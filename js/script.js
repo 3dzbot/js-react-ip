6799f78baf4c80bb09f73d6c4b1a5f480c523403
3dzbot <zoduaks@gmail.com>
2024-02-13 11:10:21 +0200

    79 - minor fix. add db.json

--- js/script.js
+++ js/script.js
@@ -123,7 +123,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		modal.classList.add('show');
 		modal.classList.remove('hide');
 		document.body.style.overflow = 'hidden';
-		clearInterval(modalTimerId);
+		// clearInterval(modalTimerId);
 	}
 
 	function hideModal() {
