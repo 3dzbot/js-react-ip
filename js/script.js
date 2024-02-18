ad18691e19770efcd8dae790152e004cb738bcec
3dzbot <zoduaks@gmail.com>
2024-02-19 00:46:15 +0200

    97 - калькулятор калорий

--- js/script.js
+++ js/script.js
@@ -387,7 +387,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			slideIndex = slideTo;
 
 			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
-			console.log(offset);
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
 			setActiveDot(slideIndex);
@@ -421,4 +420,71 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 	plusSlides(1);
 	// })
 
+	//Calc
+
+	const calcResult = document.querySelector('.calculating__result span');
+	let sex = 'female', height, weight, age, ratio = 1.375;
+
+	function calcTotal() {
+		console.log(sex, height, weight, age, ratio)
+		if (!sex || !height || !weight || !age || !ratio) {
+			calcResult.textContent = '____';
+			return;
+		}
+
+		if (sex === 'female') {
+			calcResult.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+		} else {
+			calcResult.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+		}
+	}
+
+	calcTotal();
+
+	function getStaticInformation(parentSelector, activeClass) {
+		const elements = document.querySelectorAll(`${parentSelector} div`);
+
+		elements.forEach(elem => {
+			elem.addEventListener('click', e => {
+					const target = e.target;
+
+					if (target.getAttribute('data-ratio')) {
+						ratio = +target.getAttribute('data-ratio');
+					} else {
+						sex = target.getAttribute('id');
+					}
+
+					elements.forEach(elem => {
+						elem.classList.remove(activeClass)
+					})
+					target.classList.add(activeClass);
+
+					calcTotal();
+				})
+		})
+	}
+
+	getStaticInformation('#gender', 'calculating__choose-item_active');
+	getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+	function getDynamicInformation(selector) {
+		const input = document.querySelector(selector);
+
+		input.addEventListener('input', () => {
+			switch (input.getAttribute('id')) {
+				case 'height':
+					height = +input.value; break;
+				case 'weight':
+					weight = +input.value; break;
+				case 'age':
+					age = +input.value; break;
+			}
+
+			calcTotal();
+		})
+	}
+
+	getDynamicInformation('#height');
+	getDynamicInformation('#weight');
+	getDynamicInformation('#age');
 });
